4e79ba5c11e3be93255f745e757b274db63fb6ac
Connor <61102529+Metater@users.noreply.github.com>
2022-02-01 10:49:31 -0500

    Making code cleaner

--- TurrisAuth/TurrisAuthData.cs
+++ TurrisAuth/TurrisAuthData.cs
@@ -2,5 +2,41 @@
 
 public class TurrisAuthData
 {
+    public string ServerKey { get; private set; }
+    public string ClientKey { get; private set; }
 
+    public string GameCodesPath { get; private set; }
+    public string AccountsPath { get; private set; }
+
+    public object gameCodesLock = new();
+    // <gameCodes>
+    public List<string> GameCodes { get; private set; } = new();
+
+    public object accountsLock = new();
+    // <username, passwordHash>
+    public Dictionary<string, string> Accounts { get; private set; } = new();
+
+    public object serverLock = new();
+    // <server>
+    public List<TurrisServer> Servers { get; private set; } = new();
+    // <>
+    public ConcurrentDictionary<string, TurrisServer> ServerCache { get; private set; } = new();
+
+    private bool queuedSaveGameCodes = false;
+    private bool queuedSaveAccounts = false;
+
+    public TurrisAuthData()
+    {
+        GameCodesPath = Directory.GetCurrentDirectory() + "/gameCodes.turris";
+        AccountsPath = Directory.GetCurrentDirectory() + "/accounts.turris";
+
+
+    }
+
+    public async Task Load()
+    {
+        ServerKey = await File.ReadAllTextAsync(Directory.GetCurrentDirectory() + "/serverKey.secret");
+        ClientKey = await File.ReadAllTextAsync(Directory.GetCurrentDirectory() + "/clientKey.secret");
+
+    }
 }
